9f06e24da20e34d6761491a1e846dbd2bddd749a
DanAlbul <daniel.albul@dochq.co.uk>
2024-12-21 21:59:27 +0200

    implement few more tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const appearances = {};
+
+  domains.forEach((domain) => {
+    const parts = domain.split('.').reverse();
+    let current = '';
+
+    parts.forEach((part) => {
+      current += `.${part}`; // Build the hierarchical DNS
+      appearances[current] = (appearances[current] || 0) + 1; // Count occurrences
+    });
+  });
+
+  return appearances;
 }
 
 module.exports = {
